14fda69a22baeb41a8eccb583c568ef8738644f2
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-22 14:37:37 -0400

    feat: view snaps to project header when project is made visible

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 {
   const projectButtons = document.getElementById(`project-buttons`)
 
+  const projectsHeader = document.getElementById(`projects-header`)
+
   projectButtons.addEventListener(`click`, evt => {
     evt.stopPropagation()
+    projectsHeader.scrollIntoView(true)
     projectButtons.classList.add(`gone`)
     const ele = document.getElementById(evt.target.value)
     ele.classList.remove(`gone`)
@@ -13,6 +16,7 @@
   document.getElementsByTagName(`body`)[0].addEventListener(`click`, () => {
     const visibles = [...document.getElementsByClassName(`visible`)]
     if(visibles.length > 0){
+      projectsHeader.scrollIntoView(true)
       visibles.forEach(ele => {
         ele.classList.remove(`visible`)
         ele.classList.add(`hidden`)
