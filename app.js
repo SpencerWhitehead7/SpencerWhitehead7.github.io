03d0918bc6dd7d86ffa4400fe02558c17cb0d841
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-02 22:35:30 -0400

    feat: animations cancel and restart on focus change

--- app.js
+++ app.js
@@ -37,6 +37,9 @@
   // Base element
   const banner = document.getElementById(`banner`)
 
+  // Used to cancel all scheduled timeouts when page loses focus
+  let timeouts = []
+
   // Adds/removes one character to/from base string each time it runs. If it has run fewer times than the length of the string being altered (excluding blank spaces, for easier random selection of characters to replace), it calls itself again after a delay of speed ms.
   const fillOrRemoveChars = (counter, loopLength, baseStr, blanks, domEle, speed, endStr) => {
     if(counter < loopLength){ // ends chain of setTimeout()s if every letter has been replaced
@@ -57,7 +60,7 @@
       domEle.innerHTML = newHTML
       counter++
 
-      setTimeout(() => fillOrRemoveChars(counter, loopLength, baseStr, blanks, domEle, speed, endStr), speed) // Calls itself again after speed ms pause
+      timeouts.push(setTimeout(() => fillOrRemoveChars(counter, loopLength, baseStr, blanks, domEle, speed, endStr), speed)) // Calls itself again after speed ms pause
     }
   }
 
@@ -65,7 +68,7 @@
   const generateStr = (endStr, domEle, speed, dispTime, loopLength, baseStr = ``) => {
     const blanks = new Array(endStr.length).fill(` `) // creates the array functions will fill with the str and empty
     fillOrRemoveChars(0, loopLength, baseStr, blanks, domEle, speed, endStr)
-    setTimeout(() => fillOrRemoveChars(0, loopLength, baseStr, blanks, domEle, speed), (speed * loopLength) + dispTime) // timer based on how long it takes the fill animation to fill in every non-space character and wait a to display them
+    timeouts.push(setTimeout(() => fillOrRemoveChars(0, loopLength, baseStr, blanks, domEle, speed), (speed * loopLength) + dispTime)) // timer based on how long it takes the fill animation to fill in every non-space character and wait a to display them
   }
 
   // Calls generateStr once for each passed-in display string. All the calls are initialized at once, but execute after a delay calculated to fit the animation
@@ -75,25 +78,43 @@
     for(let i = 0; i < rest.length; i++){
       const endStr = rest[i]
       const loopLength = endStr.split(``).filter(char => char !== ` `).length
-      setTimeout(() => generateStr(endStr, domEle, speed, dispTime, loopLength, baseStr), timer)
+      timeouts.push(setTimeout(() => generateStr(endStr, domEle, speed, dispTime, loopLength, baseStr), timer))
       timer += (speed * loopLength * 2) + dispTime + blankTime // timer is based on how long it takes generateStr to go through a full cycle
     }
     // console.log((rest.length * (dispTime + blankTime)) + (rest.join(``).length * speed)) // use to find APPROXIMATE (and on the small side) amount of time for setInterval if you change the input strings
   }
 
-  // Invoked as soon as the page loads
-  bannerGimmick(banner, 50, 2000, 500,
+  // Utility function to just run the bannerGimmick with my preferred inputs
+  const runAnimation = () => bannerGimmick(banner, 50, 2000, 500,
     `Spencer Whitehead`,
     `a Full Stack Software Engineer`,
     `someone who loves programming`,
     `always learning and developing my skills`)
 
-  // Loops the animation after that first invocation of bannerGimmick
-  setInterval(() => bannerGimmick(banner, 50, 2000, 500,
-    `Spencer Whitehead`,
-    `a Full Stack Software Engineer`,
-    `someone who loves programming`,
-    `always learning and developing my skills`), 20000) // Tragically, this delay between plays of the animation runner is hardcoded
+  // Utility function to loop bannerGimmick with my preferred inputs and clear the timeouts (all the scheduled ones should already by complete)
+  const loopAnimation = fullLoopTime => setInterval(() => {
+    runAnimation()
+    timeouts = []
+  }, fullLoopTime)
+
+  // Invoked as soon as the page loads to run animation once and start the loop
+  runAnimation()
+  let cancel = loopAnimation(20000)
+
+  // Ends/restarts animation when window loses/gains focus. The text quickly turns to slush without this due to browser behavior on focus change
+  const endOrRestartAnimation = () => {
+    if(document.hidden){ // loses focus; cancels the animation loop, cancels all already scheduled animation timeouts, resets banner HTML
+      clearInterval(cancel)
+      timeouts.forEach(timeout => clearTimeout(timeout))
+      banner.innerHTML = `<h1>Hi,&nbsp;<wbr>I'm</h1>`
+    }else{ // gains focus; runs the animation once and restarts the loop
+      runAnimation()
+      cancel = loopAnimation(20000)
+    }
+  }
+
+  // Actually listens for gaining/losing focus, calls handleChange function
+  document.addEventListener(`visibilitychange`, endOrRestartAnimation, false)
 
   // GIMMICKY HEADEAR END
 }
