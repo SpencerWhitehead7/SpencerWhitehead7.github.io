41de7e76a566d7067c06cd879c88328f7ccaa322
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-04 16:37:09 -0400

    edit: lint

--- buttonAnimation.js
+++ buttonAnimation.js
@@ -7,7 +7,7 @@
     evt.stopPropagation()
     const possibleEle = document.getElementById(evt.target.parentElement.value)
     const ele = possibleEle ? possibleEle : document.getElementById(evt.target.value) // This little nightmare is to fix FF's targeting of button vs Chrome's targeting of inner image
-    if(ele){ // deals with the weird edge case where the button floats up and the clicked ele registers as null and everything else errors out
+    if (ele) { // deals with the weird edge case where the button floats up and the clicked ele registers as null and everything else errors out
       projectsHeader.scrollIntoView(true)
       projectButtons.classList.add(`gone`)
       ele.classList.remove(`gone`)
@@ -19,7 +19,7 @@
   body.addEventListener(`click`, evt => {
     evt.stopPropagation()
     const visibles = [...document.getElementsByClassName(`visible`)]
-    if(visibles.length > 0){
+    if (visibles.length > 0) {
       projectsHeader.scrollIntoView(true)
       visibles.forEach(ele => {
         ele.classList.remove(`visible`)
